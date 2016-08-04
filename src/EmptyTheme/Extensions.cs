4496ffa958265da8e671546b8774d614cca4bdbe
Matthias Auswöger <matthias.auswoeger@gmx.net>
2016-08-04 16:51:41 +0200

    Initial commit

---
+++ src/EmptyTheme/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.FileProviders;
+
+namespace Theme
+{
+    public static class ThemeExtensions
+    {
+        public static void AddTheme(this IServiceCollection services)
+        {
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.FileProviders.Add(new EmbeddedFileProvider(typeof(ThemeExtensions).GetTypeInfo().Assembly, nameof(Theme)));
+            });
+        }
+
+
+        public static void UseTheme(this IApplicationBuilder app)
+        {
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new EmbeddedFileProvider(typeof(ThemeExtensions).GetTypeInfo().Assembly, nameof(Theme) + ".wwwroot")
+            });
+        }
+    }
+}
---
+++ src/EmptyTheme/gulpfile.js
@@ -0,0 +1,20 @@
+﻿/// <binding BeforeBuild='cleanup, copy' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+
+gulp.task('cleanup', function () {
+    gulp.src('./wwwroot/css', { read: false }).pipe(clean());
+    gulp.src('./wwwroot/fonts', { read: false }).pipe(clean());
+    gulp.src('./wwwroot/js', { read: false }).pipe(clean());
+});
+
+gulp.task('copy', function () {
+    gulp.src('./node_modules/bootstrap/dist/css/*.min.css').pipe(gulp.dest('./wwwroot/css'));
+    gulp.src('./node_modules/bootstrap/dist/fonts/*').pipe(gulp.dest('./wwwroot/fonts'));
+    gulp.src('./node_modules/bootstrap/dist/js/*.min.js').pipe(gulp.dest('./wwwroot/js'));
+});
