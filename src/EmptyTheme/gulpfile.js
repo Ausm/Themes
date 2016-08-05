51d26966b9690b608b3c41aee29dc7fdbfdffb23
Matthias Auswöger <matthias.auswoeger@gmx.net>
2016-08-05 16:05:08 +0200

    Fixing the missing call of gulp after npm install.

--- src/EmptyTheme/gulpfile.js
+++ src/EmptyTheme/gulpfile.js
@@ -1,20 +1,18 @@
-﻿/// <binding BeforeBuild='cleanup, copy' />
-/*
-This file in the main entry point for defining Gulp tasks and using Gulp plugins.
-Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
-*/
-
 var gulp = require('gulp');
-var clean = require('gulp-clean');
+var clean = require('gulp-dest-clean');
 
-gulp.task('cleanup', function () {
-    gulp.src('./wwwroot/css', { read: false }).pipe(clean());
-    gulp.src('./wwwroot/fonts', { read: false }).pipe(clean());
-    gulp.src('./wwwroot/js', { read: false }).pipe(clean());
+gulp.task('default', ['copy']);
+
+gulp.task('copy', ['copy:css', 'copy:js', 'copy:fonts']);
+
+gulp.task('copy:css', ['cleanup'], function () {
+    return gulp.src('./node_modules/bootstrap/dist/css/*.min.css').pipe(gulp.dest('./wwwroot/css')).pipe(clean('./wwwroot/css'));
 });
 
-gulp.task('copy', function () {
-    gulp.src('./node_modules/bootstrap/dist/css/*.min.css').pipe(gulp.dest('./wwwroot/css'));
-    gulp.src('./node_modules/bootstrap/dist/fonts/*').pipe(gulp.dest('./wwwroot/fonts'));
-    gulp.src('./node_modules/bootstrap/dist/js/*.min.js').pipe(gulp.dest('./wwwroot/js'));
+gulp.task('copy:fonts', ['cleanup'], function () {
+    return gulp.src('./node_modules/bootstrap/dist/fonts/*').pipe(gulp.dest('./wwwroot/fonts')).pipe(clean('./wwwroot/fonts'));
+});
+
+gulp.task('copy:js', ['cleanup'], function () {
+    return gulp.src('./node_modules/bootstrap/dist/js/*.min.js').pipe(gulp.dest('./wwwroot/js')).pipe(clean('./wwwroot/js'));
 });
