6f065e92683f1ff7f28078f810d83877d367d537
Matthias Auswöger <matthias.auswoeger@gmx.net>
2016-08-10 21:29:03 +0200

    The empty theme project name has changed, so the build script should either.

--- Build.ps1
+++ Build.ps1
@@ -61,4 +61,4 @@ $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
 exec { & dotnet test .\test\Ausm.EmptyTheme.Test -c Release }
 
-exec { & dotnet pack .\src\EmptyTheme -c Release -o .\artifacts --version-suffix=$revision }
+exec { & dotnet pack .\src\Ausm.EmptyTheme -c Release -o .\artifacts --version-suffix=$revision }
