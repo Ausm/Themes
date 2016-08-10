83e9f086faeff12edeba7368b3d63c0411ade149
Matthias Auswöger <matthias.auswoeger@gmx.net>
2016-08-10 19:51:46 +0200

    Rename project, add a test for the EmptyTheme.

---
+++ test/Ausm.EmptyTheme.Test/Fixtures/TestServerFixture.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Http;
+using Ausm.EmptyTheme;
+
+namespace ObjectStore.Test.Identity.Fixtures
+{
+    public class TestServerFixture : IDisposable
+    {
+        TestServer _server;
+        HttpClient _client;
+        RequestDelegate _function;
+
+        public TestServerFixture()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                .ConfigureServices(services => services.AddTheme())
+                .Configure(
+                    app => app.UseTheme()
+                ));
+            _client = _server.CreateClient();
+        }
+
+        public HttpClient Client => _client;
+
+        public void Dispose()
+        {
+            _server.Dispose();
+            _client.Dispose();
+            _server = null;
+            _client = null;
+        }
+    }
+}
---
+++ test/Ausm.EmptyTheme.Test/Tests/IdentityTests.cs
@@ -0,0 +1,60 @@
+﻿using Xunit;
+using System;
+using System.Linq;
+using Xunit.Abstractions;
+using System.Threading.Tasks;
+using ObjectStore.Test.Identity.Fixtures;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace ObjectStore.Test.Identity
+{
+    public class IdentityTests : IClassFixture<TestServerFixture>
+    {
+        #region Fields
+        ITestOutputHelper _output;
+        TestServerFixture _fixture;
+        #endregion
+
+        #region Constructor
+        public IdentityTests(ITestOutputHelper output, TestServerFixture fixture)
+        {
+            _output = output;
+            _fixture = fixture;
+        }
+        #endregion
+
+        #region Tests
+        [Theory, MemberData(nameof(PagesPresent))]
+        public async Task TestSignInSuccess(string url, string expectedHash)
+        {
+            byte[] buffer = await _fixture.Client.GetByteArrayAsync(url);
+
+            SHA1 sha = SHA1.Create();
+            string hash = Convert.ToBase64String(sha.ComputeHash(buffer));
+
+            Assert.Equal(expectedHash, hash);
+        }
+        #endregion
+
+
+        #region MemberData definitions
+        public static TheoryData<string, string> PagesPresent
+        {
+            get
+            {
+                TheoryData<string, string> returnValue = new TheoryData<string, string>();
+                returnValue.Add("/css/bootstrap.min.css", "ZSfYvz4ek2i6uMe2D1a8Afo6/Wg=");
+                returnValue.Add("/css/bootstrap-theme.min.css", "glZXU3T0MEdr3NSd6Yx3mQIpzjE=");
+                returnValue.Add("/fonts/glyphicons-halflings-regular.eot", "hrb2K3hT5n0+Y19lEqWl78WOo8M=");
+                returnValue.Add("/fonts/glyphicons-halflings-regular.svg", "3lGoSUGAptsHSvLe4jg/CjY8Wwg=");
+                returnValue.Add("/fonts/glyphicons-halflings-regular.ttf", "RLwYUPVwlyJnsWmuGPHLBrYR/6I=");
+                returnValue.Add("/fonts/glyphicons-halflings-regular.woff", "J45JqG5jTabyoC87R92dKo8mIQ8=");
+                returnValue.Add("/fonts/glyphicons-halflings-regular.woff2", "yjW2l9mcrk0bYPLWD803dxmH6wc=");
+                returnValue.Add("/js/bootstrap.min.js", "QwpEPXSDD+m+Ju/KQx9EjBs3QPk=");
+                return returnValue;
+            }
+        }
+        #endregion
+    }
+}
