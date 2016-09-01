3d58a0b73192703d5b995c3ec6ec53bdc9d5c1c3
Matthias Auswöger <matthias.auswoeger@gmx.net>
2016-09-01 19:18:55 +0200

    Add a sample project to show how to use the themes.

---
+++ sample/Sample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Sample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IConfigurationRoot hostingConfig = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("hosting.json", optional: true)
+                    .AddCommandLine(args)
+                    .Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseConfiguration(hostingConfig)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ sample/Sample/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Ausm.ThemeWithMenuAndIdentity;
+using Microsoft.Extensions.Configuration;
+
+namespace Sample
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
+        IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<ThemeOptions>(themeOptions => {
+                themeOptions.StaticMenuItems = new IMenuItem[] {
+                    new MenuItem("Home", controller: "Home", action: "Index"),
+                    new SeparatorItem(),
+                    new MenuItem("Links",
+                        new MenuItem("Google", url:"http://www.google.at"),
+                        new MenuItem("Disabled", url:"http://www.youtube.com") { IsEnabled = false },
+                        new SeparatorItem(),
+                        new MenuItem("Facebook", url:"http://www.facebook.com"))
+                };
+
+                themeOptions.DynamicMenuItems = () => null;
+            });
+
+            services.AddTheme(Configuration.GetConnectionString("DefaultConnection"));
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseTheme();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
